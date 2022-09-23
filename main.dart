7a1204e50d6e6e763b89eefc76b4fb8b2b1ea6a8
Alok Kumar Maurya <alokkumarmaurya5556@gmail.com>
2022-09-23 16:39:19 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:open_auth/pages/ui.dart';
 
+//install all the packages as needed
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
